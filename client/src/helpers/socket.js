9de6b6d7ee0c192b1a6caa1dafe368d8f36120bf
vince19972 <vince19972@gmail.com>
2019-03-07 11:38:55 -0500

    [socket] update url based on env

--- client/src/helpers/socket.js
+++ client/src/helpers/socket.js
@@ -1,7 +1,10 @@
 const io = require('socket.io-client')
 
 export default () => {
-	const socket = io.connect('http://localhost:8000', {reconnection: true})
+	const serverURL = process.env.NODE_ENV === 'development'
+		? 'http://localhost:8000'
+		: 'https://mobilelab-websocket.herokuapp.com/'
+	const socket = io.connect(serverURL, {reconnection: true})
 
 	const play = (player) => {
 		socket.emit('*', player.id, player.direction)
